70a76b447b8ce2adb1305190976e306c1832a5a7
illupovero <davideluporini1996@gmail.com>
2022-01-10 16:59:44 +0100

    added  for cicle

--- js/script.js
+++ js/script.js
@@ -7,9 +7,26 @@ console.log('hello world')
 // controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
 
 
-const list = ['aaa@gmail.com', 'bbb@gmail.com'];
+const list = ['aaa@gmail.com', 'bbb@gmail.com', 'ccc@gmail.com', 'aaa@gmail.com'];
 let email = prompt("Inserisci la tua email!");
+console.log(email);
 
+let validEmail = 0;
+
+for (i = 0; i < list.length; i++) {
+    // se l'email coincide
+    if (email == list[i]) {
+        validEmail++;
+    }
+}
+
+console.log(validEmail);
+
+if (validEmail >= 1) {
+    console.log(`la tua email (${email}) è valida`);
+} else {
+    console.log(`la tua email (${email}) non è valida`);
+}
 
 
 
