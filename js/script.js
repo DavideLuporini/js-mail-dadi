ee1bade8f21f64f70304ae7d65d8bab6000c96fb
illupovero <davideluporini1996@gmail.com>
2022-01-10 16:00:32 +0100

    added array ,  added prompt to ask the emai

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log('hello world')
+
+
+// Mail
+// Crea un array di email;
+// Chiedi all’utente la sua email.
+// controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
+
+
+const list = ['aaa@gmail.com', 'bbb@gmail.com'];
+let email = prompt("Inserisci la tua email!");
+
+
+
+
+
+//  Gioco dei dadi
+//  Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Stampare in console i tiri di dado e il risultato.
